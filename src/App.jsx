fc8fe01333549bbaab1a05a6a9c97254a81f5cc8
marwa <marwajawad19@gmail.com>
2023-12-19 15:50:32 +0400

    fix comment

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ function App() {
       <LoopPackages />
       <LoopFeatures />
       <PrincessYacht />
-      <MultiCarousel />
-      {/* <Testimonials /> */}
+      {/* <MultiCarousel /> */}
+      <Testimonials />
       <Explore />
       <Footer />
     </>
